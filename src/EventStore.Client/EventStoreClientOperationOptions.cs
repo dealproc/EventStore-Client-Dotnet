66fdba8ccaaf17dbab42b6ba4799b2b35054005f
pgermishuys <pieter@pieterg.com>
2020-05-04 15:14:45 +0200

    Introduce the ability for the consumer to decide to throw on failure
    This behaviour is currently only applied when performing a write.

--- src/EventStore.Client/EventStoreClientOperationOptions.cs
+++ src/EventStore.Client/EventStoreClientOperationOptions.cs
@@ -4,13 +4,17 @@
 namespace EventStore.Client {
 	public class EventStoreClientOperationOptions {
 		public TimeSpan? TimeoutAfter { get; set; }
+		public bool ThrowOnFailure { get; set; }
+
 		public static EventStoreClientOperationOptions Default => new EventStoreClientOperationOptions {
 			TimeoutAfter = TimeSpan.FromSeconds(5),
+			ThrowOnFailure = true
 		};
 
 		public EventStoreClientOperationOptions Clone() =>
 			new EventStoreClientOperationOptions {
-				TimeoutAfter = TimeoutAfter
+				TimeoutAfter = TimeoutAfter,
+				ThrowOnFailure = true
 			};
 	}
 }
